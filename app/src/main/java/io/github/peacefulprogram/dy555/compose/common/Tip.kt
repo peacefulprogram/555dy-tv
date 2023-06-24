faf0013f4671b5181ba67f6705f87b5dcb5aaa13
peacefulprogram <jw4273@qq.com>
2023-06-24 19:17:37 +0800

    修改Loading和ErrorTip样式

--- app/src/main/java/io/github/peacefulprogram/dy555/compose/common/Tip.kt
+++ app/src/main/java/io/github/peacefulprogram/dy555/compose/common/Tip.kt
@@ -1,9 +1,11 @@
 package io.github.peacefulprogram.dy555.compose.common
 
-import androidx.compose.foundation.focusable
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -13,8 +15,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.tv.material3.Border
 import androidx.tv.material3.Button
+import androidx.tv.material3.ButtonDefaults
+import androidx.tv.material3.ButtonScale
 import androidx.tv.material3.ExperimentalTvMaterial3Api
+import androidx.tv.material3.MaterialTheme
 import androidx.tv.material3.Text
 import io.github.peacefulprogram.dy555.R
 
@@ -27,6 +34,7 @@ fun Loading(text: String = "Loading"): Unit {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         CircularProgressIndicator()
+        Spacer(modifier = Modifier.height(10.dp))
         Text(text = text)
     }
 }
@@ -43,10 +51,25 @@ fun ErrorTip(message: String, retry: () -> Unit = { }) {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(text = message)
+        Spacer(modifier = Modifier.height(10.dp))
         Button(
-            onClick = retry, modifier = Modifier
-                .focusRequester(focusRequester)
-                .focusable()
+            onClick = retry,
+            modifier = Modifier
+                .focusRequester(focusRequester),
+            border = ButtonDefaults.border(
+                focusedBorder = Border(
+                    BorderStroke(2.dp, MaterialTheme.colorScheme.border),
+                    shape = MaterialTheme.shapes.extraLarge
+                )
+            ),
+            shape = ButtonDefaults.shape(shape = MaterialTheme.shapes.extraLarge),
+            scale = ButtonScale.None,
+            colors = ButtonDefaults.colors(
+                containerColor = MaterialTheme.colorScheme.surfaceVariant.copy(alpha = 0.8f),
+                contentColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.8f),
+                focusedContainerColor = MaterialTheme.colorScheme.surfaceVariant.copy(alpha = 0.8f),
+                focusedContentColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.8f)
+            )
         ) {
             Text(text = stringResource(R.string.button_retry))
         }
